f2abb10c33f04d9effa53f6a914e3d59e6641524
Galleazzo <paulogalleazzo1@gmail.com>
2024-11-07 10:14:03 -0300

    feat: remove desnececery methods in service

--- src/main/java/com/br/web/v1/service/AnimalsService.java
+++ src/main/java/com/br/web/v1/service/AnimalsService.java
@@ -44,29 +44,6 @@ public class AnimalsService {
         return this.modelMapper.map(animal, AnimalsDTO.class);
     }
 
-    public AnimalsDTO save(AnimalsDTO animalsDTO) {
-        Animals animals = new Animals();
-        Date registrationDate = new Date();
-
-        if(animalsDTO.getId() != null) {
-            animals = this.animalsRepository.getById(animalsDTO.getId());
-        }
-        if(animalsDTO.getId() == null) {
-            animals.setRegistrationDate(registrationDate);
-        }
-        this.convertDtoToObject(animalsDTO, animals);
-
-        return this.modelMapper.map(this.animalsRepository.save(animals), AnimalsDTO.class);
-    }
-
-    public void deleteAnimal(Long id) throws Exception {
-        try {
-            animalsRepository.deleteById(id);
-        } catch (Exception e) {
-            throw new Exception("Erro ao deletar o animal: " + e.getMessage());
-        }
-    }
-
     public List<AnimalsDTO> getAll() {
         if (cachedAnimalList == null) {
             loadCache();
@@ -93,52 +70,6 @@ public class AnimalsService {
     private void invalidateCache() {
         cacheVersion = UUID.randomUUID();
         cachedAnimalList = null;
-    }
-
-    public Set<ImageAnimalModel> uplodImage(MultipartFile[] multipartFiles) throws IOException {
-        Set<ImageAnimalModel> imageModels = new HashSet<>();
-
-        for(MultipartFile file: multipartFiles) {
-            ImageAnimalModel imageModel = new ImageAnimalModel(
-                    file.getOriginalFilename(),
-                    file.getContentType(),
-                    file.getBytes());
-            imageModels.add(imageModel);
-        }
-        return imageModels;
-    }
-
-    public void changeActive(Long id, Date adoptionDate) {
-        Animals animal = this.animalsRepository.getById(id);
-        animal.setActive(!animal.getActive());
-
-        if (animal.getActive() == false) {
-            LocalDateTime now = LocalDateTime.now();
-            Date date = Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
-            animal.setAdoptionDate(date);
-
-            animal.setAdoptionDate(adoptionDate);
-        } else {
-            animal.setAdoptionDate(null);
-        }
-        this.animalsRepository.save(animal);
-    }
-
-    private Animals convertDtoToObject(AnimalsDTO animalsDTO, Animals animals) {
-        animals.setName(animalsDTO.getName());
-        animals.setInstagramURL(animalsDTO.getInstagramURL());
-        animals.setAnimalAge(animalsDTO.getAnimalAge());
-        animals.setAnimalType(animalsDTO.getAnimalType());
-        animals.setRace(animalsDTO.getRace());
-        animals.setSize(animalsDTO.getSize());
-        animals.setDescription(animalsDTO.getDescription());
-        animals.setPriority(animalsDTO.getPriority());
-        animals.setAnimalImage(animalsDTO.getAnimalImage());
-        animals.setAnimalSex(animalsDTO.getAnimalSex());
-        animals.setAdoptionDate(null);
-        animals.setActive(true);
-
-        return animals;
     }
 
 }
